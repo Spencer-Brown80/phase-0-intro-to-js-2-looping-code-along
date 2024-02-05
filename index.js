09de5995c600d6a19c9e14992036aa53bc695b8e
Spencer <spinsab@gmail.com>
2024-02-05 17:05:14 -0500

    array and countdown lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+const finishedCards = []
+
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    // console.log(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    console.log(finishedCards);
+   finishedCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    console.log(finishedCards);
+    // debugger;
+  }
+    return finishedCards;
+  
+}
+
+
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+function countDown() {
+    let countDown = 10;
+    while (countDown >=0) {
+        console.log(countDown--);
+    }
+}
